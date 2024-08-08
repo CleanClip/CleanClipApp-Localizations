e0ba3715c6e27103d4e7bf63fc5d84c31fdcdbf2
sintone <sintone@gokoding.com>
2024-08-08 20:44:44 +0800

    update translation scripts
    Signed-off-by: sintone <sintone@gokoding.com>

--- translation.py
+++ translation.py
@@ -1,7 +1,8 @@
 import os
 import git
-from googletrans import Translator
+#from googletrans import Translator
 import argparse
+import openaiapi
 
 lang_codes = {
     'zh-Hans': 'zh-cn',
@@ -9,12 +10,16 @@ lang_codes = {
     'en': 'en'
 }
 
-def translate_strings(strings, target_lang):
-    translator = Translator()
+def translate_strings(strings, target_lang, provider):
+#    translator = Translator()
     translations = {}
     print(f"===========\ntranslating from en to {target_lang}")
     for key, en_string in strings.items():
-        translation = translator.translate(en_string, dest=target_lang).text
+        if provider == "openai":
+            translation = openaiapi.translate(en_string, "english", target_lang)
+        else:
+            translation = openaiapi.translate(en_string, "english", target_lang)
+#            translation = translator.translate(en_string, dest=target_lang).text
         translations[key] = translation
     return translations
 
@@ -72,7 +77,17 @@ def find_missing_strings(base_strings, target_strings):
     print(f"Found {len(missing)} missing strings")
     return missing
     
-def translate_and_write(base_file_path, base_strings_new, mapped_lang, file_path, additional_strings):
+def parse_strings_lines_to_strings(lines):
+    strings = {}
+    for line in lines:
+        if '=' in line:
+            key, value = line.split('=', 1)
+            key = key.strip().strip('"')
+            value = value.strip().strip(';').strip('"')
+            strings[key] = value
+    return strings
+    
+def translate_and_write(base_file_path, base_strings_new, base_lang, mapped_lang, file_path, additional_strings, provider):
     print(f"\nProcessing {mapped_lang} localization")
     target_strings = parse_strings_file(file_path)
     missing_strings = find_missing_strings(base_strings_new, target_strings)
@@ -83,17 +98,32 @@ def translate_and_write(base_file_path, base_strings_new, mapped_lang, file_path
         for key, value in strings_to_translate.items():
             print(f'"{key}" = "{value}";')
 
-        translated_strings = translate_strings(strings_to_translate, mapped_lang)
-        print(f"\nTranslated strings for {mapped_lang}:")
-        for key, value in translated_strings.items():
-            print(f'"{key}" = "{value}";')
+        all_translated_lines = []
+        # 每 50 项合并翻译一次
+        # strings_to_translate 数组 join 成长字符串
+        strings_list = list(strings_to_translate.items())
+        for i in range(0, len(strings_list), 50):
+            to_be_translated_lines = "\n".join([f'"{key}" = "{value}";' for key, value in strings_list[i:i+50]])
+            translated_lines = openaiapi.translate_localizable_strings(to_be_translated_lines, "english", mapped_lang)
+
+            all_translated_lines.extend(translated_lines.splitlines())
+
+#        print(f"\nTranslated lines for {mapped_lang}:")
+#        for line in all_translated_lines:
+#            print(line)
+        print(f"\nall_translated_lines: {len(all_translated_lines)}")
+        translated_strings = parse_strings_lines_to_strings(all_translated_lines)
+
+        print(f"\nTranslated strings for {mapped_lang} {len(translated_strings.items())}:")
+#        for key, value in translated_strings.items():
+#            print(f'"{key}" = "{value}";')
 
         write_translations(base_file_path, file_path, base_strings_new, target_strings, translated_strings)
         print(f"Translations written to {file_path}")
     else:
         print(f"No strings to translate for {mapped_lang}")
 
-def autotranslate(old_commit_id, new_commit_id):
+def autotranslate(old_commit_id, new_commit_id, provider):
     root_dir = os.path.join(os.getcwd(), "")
     repo = git.Repo(root_dir)
     
@@ -153,7 +183,7 @@ def autotranslate(old_commit_id, new_commit_id):
         mapped_lang = lang_codes.get(lang, lang)
         if mapped_lang.startswith('en'):
             continue
-        translate_and_write(localizable_files[en_key], base_strings_new, mapped_lang, file_path, diff_strings)
+        translate_and_write(localizable_files[en_key], base_strings_new, "english", mapped_lang, file_path, diff_strings, provider)
     
 def checkCN():
     # 找到 cn
@@ -173,21 +203,67 @@ def main():
     parser.add_argument('--target', nargs='?', const='', help='The target value')
 
     parser.add_argument('pre', nargs='?', const='', help='Pre check CN')
+    
+    parser.add_argument('--provider', nargs='?', const='', help='Translation provider')
+    parser.add_argument('--api_key', nargs='?', const='', help='Translation provider key')
+    
+    parser.add_argument('--base_lang', nargs='?', const='', help='The base lang')
+    parser.add_argument('--target_lang', nargs='?', const='', help='The target lang')
 
     # 解析命令行参数
     args = parser.parse_args()
     
     pre = args.pre
+    base = args.base_lang
+    target = args.target_lang
+    
+    apikey = args.api_key
+    if apikey:
+        openaiapi.client.api_key = apikey
+        openaiapi.client.base_url = "https://api.openai-sb.com/v1"
     
     if pre:
         checkCN()
+    elif base and target:
+        translation_from_to(base, target)
     else:
         # 访问参数值
         old_commit_id = args.prev
         new_commit_id = args.target
         
-        autotranslate(old_commit_id, new_commit_id)
+        provider = args.provider
+        if not provider:
+            provider = "openai"
+        
+        autotranslate(old_commit_id, new_commit_id, provider)
     
+
+def translation_from_to(base_lang, target_lang):
+#    base_file_path, base_strings_new, mapped_lang, file_path, additional_strings, provider
+    root_dir = os.path.join(os.getcwd(), "")
+    localizable_files = find_localizable_files(root_dir)
+    # Find the English file (it could be 'en', 'en-US', 'en-GB', etc.)
+    base_key = next((lang for lang in localizable_files.keys() if lang.startswith(base_lang)), None)
+    target_key = next((lang for lang in localizable_files.keys() if lang.startswith(target_lang)), None)
     
+    if not base_key:
+        print("Error: English localization file not found.")
+        print("Available languages:", ", ".join(localizable_files.keys()))
+        return
+    
+    if not target_key:
+        print("Error: target localization file not found.")
+        print("Available languages:", ", ".join(localizable_files.keys()))
+        return
+    
+    base_strings_new = parse_strings_file(localizable_files[base_key])
+    target_lang = lang_codes.get(target_lang, target_lang)
+    translate_and_write(localizable_files[base_key], base_strings_new, base_lang, target_lang, localizable_files[target_key], {"app": "Application",
+"ignore_or_not": "Ignore",
+"bundle_id": "Bundle Id",
+"select_all": "Select All",
+"unselect_all": "Unselect All",
+"acknowledgements": "Acknowledgements"}, "openai")
+
 if __name__ == "__main__":
     main()
